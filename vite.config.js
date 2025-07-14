d20bbfdfab1ae579a15d0517d2a8de6908ffaa80
Sanyam Mahajan <mahajansanyam07@gmail.com>
2025-07-14 16:39:09 +0530

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Sanyam-Portfolio/', // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
-})
+});
